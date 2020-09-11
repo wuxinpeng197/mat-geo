dbe7624c120d7c9d3d27474b9553e1861f50e187
wuxinpeng197 <xinpengwu193@gmail.com>
2020-09-11 16:46:00 +1000

    Update

--- server.js
+++ server.js
@@ -32,11 +32,12 @@ const server = new ApolloServer({
 
 // DEFAULT 
 const app = express();
-app.use(express.static('client/build'))
+app.use(express.static('client/build'));
+
 if( process.env.NODE_ENV === 'production' ){
     const path = require('path');
     app.get('/*',(req,res)=>{
-        res.sendfile(path.resolve(__dirname,'../client','build','index.html'))
+        res.sendfile(path.resolve(__dirname, 'client','build','index.html'))
     })
 }
 
